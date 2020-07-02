6020c9913b0bf3e60b1551a77c91d3cb8c8149f8
XUSAMXU <804600733@qq.com>
2020-07-02 18:03:00 +0800

    appjs -> homepage

--- app.js
+++ app.js
@@ -1,59 +1,17 @@
 const http = require('http');
+const superagent = require('superagent')
+const cheerio = require('cheerio')
 
-const homePage = `
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <style type="text/css">
-        * {
-            padding: 0;
-            margin: 0;
-        }
-        body {
-            padding: 30px 0;
-            text-align: center;
-            font-size: 16px;
-            background-color: #333;
-        }
-        h1,h2 {
-            color: #fff;
-        }
-        nav {
-            margin-top: 20px;
-        }
-        a {
-            color: #ccc;
-            cursor: pointer;
-        }
-        a:hover {
-            text-decoration: underline;
-        }
-    </style>
-</head>
-
-<body>
-    <h1>Nodejs部署示例项目</h1>
-    <h2>项目部署上线示例</h2>
-    <nav>
-        <ul>
-            <li><a>列表</a></li>
-        </ul>
-    </nav>
-</body>
-
-</html>
-`
-
-http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type','text/html');
-    res.end(homePage);
+const url = 'http://jianshu.com'
+const homePage = 
+ superagent.get( url ).end( err,res => {
+	 if(err) return new Error(err)
+	 return res.text
+ })
+http.createServer((req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/html');
+	res.end(homePage);
 }).listen(3000, () => {
-    console.log('Sever Running On 3000:');
+	console.log('Sever Running On 3000:');
 })
-
